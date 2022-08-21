8cbcb24bbd25c30b973fc8a9ce03d66d64b2a88e
zhn <63408014+newzhn@users.noreply.github.com>
2022-08-21 21:58:22 +0800

    Update blog_admin.js

--- js/admin/blog_admin.js
+++ js/admin/blog_admin.js
@@ -5,13 +5,13 @@ $(function() {
         width   : "100%",
         height  : 550,
         syncScrolling : "single",
-        path    : "https://cdn.jsdelivr.net/gh/newzhn/blog-resource@1.0/lib/editormd/lib/"
+        path    : "../../../lib/editormd/lib/"
     });
     contentEditor2 = editormd("blog-md-content2", {
         width   : "100%",
         height  : 550,
         syncScrolling : "single",
-        path    : "https://cdn.jsdelivr.net/gh/newzhn/blog-resource@1.0/lib/editormd/lib/"
+        path    : "../../../lib/editormd/lib/"
     });
 });
 
